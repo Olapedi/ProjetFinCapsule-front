2458de1b1cae12396aab8d379a87fb5ccfa2e6b7
Ruben <rubenhamou@gmail.com>
2023-10-25 17:34:34 +0200

    mise en forme de l'affichage d'un evenement

--- app/members/events/page.tsx
+++ app/members/events/page.tsx
@@ -30,7 +30,7 @@ export default function EventView() {
             <div>
                 {`le numéro de l'événement est ${eventid}\n`}
                 {JSON.stringify(data, null, 2)}
-                <EventDisplay evtUid={eventid} title={data.title} longDescription={data.longDescription}/>
+                <EventDisplay evtUid={eventid} title={data.title} longDescription={data.longDescription} country={data.countries} city={data.cities} />
             </div>
 
             <div>
