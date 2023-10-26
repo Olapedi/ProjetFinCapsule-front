e4d67a5560afc484837e631003b9cc99641d075c
Ruben <rubenhamou@gmail.com>
2023-10-26 15:24:59 +0200

    correction image nextjs

--- components/site/eventdisplay.tsx
+++ components/site/eventdisplay.tsx
@@ -31,12 +31,10 @@ export default function EventDisplay(props : any) {
                     <div className="lg:pr-4">
                         <div className="relative overflow-hidden rounded-3xl bg-gray-900 px-6 pb-9 pt-96 shadow-2xl sm:px-12 lg:max-w-lg lg:px-8 lg:pb-8 xl:px-10 xl:pb-10">
                             
-                            <Image  className="absolute inset-0 h-full w-full object-cover"
+                            <Image  className="absolute inset-0 h-full w-full object-cover hover:scale-105 transition duration-500"
                                     src={"https://images.unsplash.com/photo-1697809462690-57bc1601f665?auto=format&fit=crop&q=80&w=1887&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"} 
-                                    width={30} height={30} 
-                                    alt="">
-
-                            </Image>
+                                    width={500} height={500} 
+                                    alt="" />
                             
                         </div>
                     </div>
