80184b5170f58d0a9360404b4f20e0480d5b040c
Ruben <rubenhamou@gmail.com>
2023-11-02 11:20:30 +0100

    update mise en forme menu burger navbar

--- components/common/globalnavbar.tsx
+++ components/common/globalnavbar.tsx
@@ -257,21 +257,21 @@ export default function GlobalNavbar() {
                 {/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800" */}
                 <Disclosure.Button
                     as="a"
-                    href="/feed"
+                    href="/feed?search=all"
                     className="block border-l-4 border-indigo-500 bg-indigo-50 py-2 pl-3 pr-4 text-base font-medium text-indigo-700"
                 >
                     Feed
                 </Disclosure.Button>
                 <Disclosure.Button
                     as="a"
-                    href="/people"
+                    href="/people?search=all"
                     className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800"
                 >
                     Membres
                 </Disclosure.Button>
                 <Disclosure.Button
                     as="a"
-                    href="/events"
+                    href="/meet?search=all"
                     className="block border-l-4 border-transparent py-2 pl-3 pr-4 text-base font-medium text-gray-600 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800"
                 >
                     Rencontres
@@ -292,14 +292,14 @@ export default function GlobalNavbar() {
                     <div className="text-base font-medium text-gray-800">{auth.displayName}</div>
                     <div className="text-sm font-medium text-gray-500">{auth.email}</div>
                     </div>
-                    <button
+                    {/* <button
                     type="button"
                     className="relative ml-auto flex-shrink-0 rounded-full bg-white p-1 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
                     >
                     <span className="absolute -inset-1.5" />
                     <span className="sr-only">Voir les notifications</span>
                     <BellIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
+                    </button> */}
                 </div>
                 <div className="mt-3 space-y-1">
                     <Disclosure.Button
