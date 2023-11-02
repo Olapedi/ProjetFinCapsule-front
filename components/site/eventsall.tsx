7ae2ee3ed358063c01dd4dbf5168fae5d511ccca
Ruben <rubenhamou@gmail.com>
2023-11-02 13:08:35 +0100

    mise en forme et correction popover superposition

--- components/site/eventsall.tsx
+++ components/site/eventsall.tsx
@@ -46,7 +46,7 @@ export default function EventsAll() {
         console.log('events', events)
     return (
         
-        <div className="bg-white py-24 sm:py-32">
+        <div className="absolute left-0 right-0 top-0 bottom-0 -z-10 bg-white py-24 sm:py-32">
         
             <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto max-w-2xl text-center">
@@ -80,12 +80,12 @@ export default function EventsAll() {
                                     <time dateTime={event.occurences[0].startDate} className="text-gray-500">
                                         {event.occurences[0].startDate.slice(0, 9+1)} - {event.occurences[0].startDate.slice(11, 15+1)}
                                     </time>
-                                    <a
+                                    {/* <a
                                         href="#"
                                         className="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
                                     >
                                     {event.occurences[0].title}
-                                    </a>
+                                    </a> */}
                                 </div>
                                 <div className="group relative">
                                     <h3 className="mt-3 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
@@ -96,7 +96,7 @@ export default function EventsAll() {
                                     </h3>
                                     <p className="mt-5 line-clamp-3 text-sm leading-6 text-gray-600">{event.occurences[0].shortDescription}</p>
                                 </div>
-                                <div className="relative mt-8 flex items-center gap-x-4">
+                                {/* <div className="relative mt-8 flex items-center gap-x-4">
 
                                     <Image className="h-10 w-10 rounded-full bg-gray-100"
                                             src={'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'}
@@ -112,7 +112,7 @@ export default function EventsAll() {
                                         </p>
                                         <p className="text-gray-600">Co-Founder / CTO</p>
                                     </div>
-                                </div>
+                                </div> */}
                             </div>
                             </article>
                         </Link>
